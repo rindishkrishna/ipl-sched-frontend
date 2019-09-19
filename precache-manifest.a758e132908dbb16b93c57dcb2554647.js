6f0a755ad92b28e43546a6a694553ad74d6e49e7
AFNAN NAVAZ <afnan@pop-os.localdomain>
2019-09-19 19:11:31 +0530

    Updates

---
+++ precache-manifest.a758e132908dbb16b93c57dcb2554647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68a9d346b3ac90d6d78f6b3a7853f90",
+    "url": "/ipl-sched-frontend/index.html"
+  },
+  {
+    "revision": "dc238574db07af38c4d0",
+    "url": "/ipl-sched-frontend/static/css/main.da03725e.chunk.css"
+  },
+  {
+    "revision": "30a886d23fc90bf66966",
+    "url": "/ipl-sched-frontend/static/js/2.497dade9.chunk.js"
+  },
+  {
+    "revision": "dc238574db07af38c4d0",
+    "url": "/ipl-sched-frontend/static/js/main.7d6dea2a.chunk.js"
+  },
+  {
+    "revision": "56302d7d72f1e9a249bc",
+    "url": "/ipl-sched-frontend/static/js/runtime~main.3d1b2ff6.js"
+  }
+]);
