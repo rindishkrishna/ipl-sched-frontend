dfd45626bc6913f0b388464ba2089d7b48558b8c
varun krishna s <varunhawk19@gmail.com>
2019-09-20 00:11:13 +0530

    first-commit

---
+++ precache-manifest.aec7d5256b76361324fb16243c99e0ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb5f27f69ce2c8891a31f24297d1fac",
+    "url": "/ipl-sched-frontend/index.html"
+  },
+  {
+    "revision": "21d5e9cc4d4ac76d0910",
+    "url": "/ipl-sched-frontend/static/css/main.da03725e.chunk.css"
+  },
+  {
+    "revision": "30a886d23fc90bf66966",
+    "url": "/ipl-sched-frontend/static/js/2.497dade9.chunk.js"
+  },
+  {
+    "revision": "21d5e9cc4d4ac76d0910",
+    "url": "/ipl-sched-frontend/static/js/main.61139e78.chunk.js"
+  },
+  {
+    "revision": "56302d7d72f1e9a249bc",
+    "url": "/ipl-sched-frontend/static/js/runtime~main.3d1b2ff6.js"
+  }
+]);
